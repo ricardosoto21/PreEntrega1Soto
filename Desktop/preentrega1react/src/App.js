d7619136c17e9920bb2283501140b4c800242fa0
RicardoSoto <ri.soto.o@gmail.com>
2023-09-14 22:07:12 -0300

    font-awesome

--- Desktop/preentrega1react/src/App.js
+++ Desktop/preentrega1react/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 
 function App() {
